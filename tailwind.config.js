373246fc638f038c3461fe3dd5bccfb7578707f1
Yousef Abd-El-Baset <87822962+Yousef-Abdelbaset@users.noreply.github.com>
2022-12-15 23:53:11 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+    fontFamily: {
+    body: ["Roboto"]
+    }
+    },
+  },
+  plugins: [],
+};
+
+
